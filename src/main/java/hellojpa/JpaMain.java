fd850cf5ce589c2662fa05755e5caed9aeac8e0b
refresh88 <ouok88@gmail.com>
2023-08-02 23:18:12 +0900

    section7 extend mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,6 +16,20 @@ public class JpaMain {
 
         try {
 
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
+
+            em.persist(movie);
+            
+            em.flush();
+            em.clear();
+
+            Item item = em.find(Item.class, movie.getId());
+            System.out.println("item = " + item);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
