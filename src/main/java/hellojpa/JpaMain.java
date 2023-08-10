52ce9f39adcf8d3ce5638cb2600c78d4bccedf84
refresh88 <115513248+refresh88@users.noreply.github.com>
2023-08-10 23:14:58 +0900

    section9 value type Collections

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,7 @@ import org.hibernate.Hibernate;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,17 +18,39 @@ public class JpaMain {
 
         try {
 
-            Address address = new Address("city", "street", "zipcode");
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+
             em.persist(member);
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
+            em.flush();
+            em.clear();
 
+            System.out.println("===============START================");
+            // 컬렉션은 기본적으로 지연로딩임.
+            Member findMember = em.find(Member.class, member.getId());
 
+            // homeCity -> newCity
+//            findMember.getAddressHistory().setCity("newCity");    setter 쓰지마라..
+            Address a = findMember.getHomeAddress();
+            // 값 타입을 바꿀거면 하나의 값만 바꾼다고 하더라도 새로 다 만들어줘야함. 교체의 개념이라고 보면됨.
+//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            // 치킨 -> 한식
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+            System.out.println("===============ADDRESS================");
+//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
 
             tx.commit();
